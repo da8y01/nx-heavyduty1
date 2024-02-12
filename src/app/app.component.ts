44ff4ba6cdb21f8c75f84dc805cf44f25e2b80f0
Daniel Benitez <da8y_01@yahoo.com>
2024-02-12 16:30:32 -0500

    feat: integrar wallet adapter.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,18 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { HdWalletMultiButtonComponent } from '@heavy-duty/wallet-adapter-material';
 
 @Component({
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, HdWalletMultiButtonComponent],
   selector: 'nx-heavyduty1-root',
   template: `
-    <header>
-      <h1>Hola, soy Bob.</h1>
+    <header class="py-8">
+      <h1 class="text-5xl text-center mb-4">Hola, soy Bob.</h1>
     </header>
+    <div class="flex justify-center">
+      <hd-wallet-multi-button></hd-wallet-multi-button>
+    </div>
   `,
 })
 export class AppComponent {
