ef5b3688f35373240e25425dbb9f6a0fd1752bca
Daniel Benitez <da8y_01@yahoo.com>
2024-02-13 13:39:18 -0500

    feat: se agrega material table en app.component .

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,28 @@
 import { Component, inject } from '@angular/core';
 import { toSignal } from '@angular/core/rxjs-interop';
 import { MatAnchor } from '@angular/material/button';
+import { MatTableModule } from '@angular/material/table';
 import { RouterModule } from '@angular/router';
 import { WalletStore } from '@heavy-duty/wallet-adapter';
 import { HdWalletMultiButtonComponent } from '@heavy-duty/wallet-adapter-material';
 import { computedAsync } from 'ngxtension/computed-async';
 import { ShyftApiService } from './shyft-api.service';
 
+export interface TransactionElement {
+  type: string;
+  source: string;
+  destination: string;
+  amount: number;
+}
+
+const ELEMENT_DATA: TransactionElement[] = [
+  {type: 'transferChecked', source: 'abcd0123', destination: 'abcd0123', amount: 1},
+  {type: 'transferChecked', source: 'abcd0123', destination: 'abcd0123', amount: 2},
+];
+
 @Component({
   standalone: true,
-  imports: [RouterModule, HdWalletMultiButtonComponent, MatAnchor],
+  imports: [RouterModule, HdWalletMultiButtonComponent, MatAnchor, MatTableModule],
   selector: 'nx-heavyduty1-root',
   template: `
     <header class="py-8">
@@ -27,11 +40,46 @@ import { ShyftApiService } from './shyft-api.service';
         }
 
         @if (accountTransactionHistory()) {
-          <div class="top-4 left-4 flex justify-center items-center gap-2">
+          <div class="top-4 left-4 flex justify-center items-center gap-2 mb-4">
             <p class="text-lg">{{ accountTransactionHistory()?.message }} | </p>
             <p class="text-lg">{{ accountTransactionHistory()?.result }} | </p>
             <p class="text-lg">{{ accountTransactionHistory()?.success }}</p>
           </div>
+        }
+
+        @if (accountTransactionHistory()) {
+          <table mat-table [dataSource]="dataSource" class="mat-elevation-z8">
+
+            <!--- Note that these columns can be defined in any order.
+                  The actual rendered columns are set as a property on the row definition" -->
+
+            <!-- Type Column -->
+            <ng-container matColumnDef="type">
+              <th mat-header-cell *matHeaderCellDef> Type </th>
+              <td mat-cell *matCellDef="let element"> {{element.type}} </td>
+            </ng-container>
+
+            <!-- Source Column -->
+            <ng-container matColumnDef="source">
+              <th mat-header-cell *matHeaderCellDef> Source </th>
+              <td mat-cell *matCellDef="let element"> {{element.source}} </td>
+            </ng-container>
+
+            <!-- Destination Column -->
+            <ng-container matColumnDef="destination">
+              <th mat-header-cell *matHeaderCellDef> Destination </th>
+              <td mat-cell *matCellDef="let element"> {{element.destination}} </td>
+            </ng-container>
+
+            <!-- Amount Column -->
+            <ng-container matColumnDef="amount">
+              <th mat-header-cell *matHeaderCellDef> Amount </th>
+              <td mat-cell *matCellDef="let element"> {{element.amount}} </td>
+            </ng-container>
+
+            <tr mat-header-row *matHeaderRowDef="displayedColumns"></tr>
+            <tr mat-row *matRowDef="let row; columns: displayedColumns;"></tr>
+          </table>
         }
       </section>
 
@@ -53,6 +101,9 @@ import { ShyftApiService } from './shyft-api.service';
   `,
 })
 export class AppComponent {
+  displayedColumns: string[] = ['type', 'source', 'destination', 'amount'];
+  dataSource = ELEMENT_DATA;
+
   private readonly _shyftApiService = inject(ShyftApiService);
   private readonly _walletStore = inject(WalletStore);
   private readonly _publicKey = toSignal(this._walletStore.publicKey$);
