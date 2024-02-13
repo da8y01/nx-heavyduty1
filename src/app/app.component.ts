1ce780d0d1335132ab861d7ebccf4a99af5de096
Daniel Benitez <da8y_01@yahoo.com>
2024-02-13 10:47:20 -0500

    feat: se agregaron dos páginas.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,22 +9,37 @@ import { ShyftApiService } from './shyft-api.service';
 
 @Component({
   standalone: true,
-  imports: [RouterModule, HdWalletMultiButtonComponent],
+  imports: [RouterModule, HdWalletMultiButtonComponent, MatAnchor],
   selector: 'nx-heavyduty1-root',
   template: `
     <header class="py-8">
-      <h1 class="text-5xl text-center mb-4">Hola, soy Bob.</h1>
-    </header>
-    <div class="flex justify-center">
-      <hd-wallet-multi-button></hd-wallet-multi-button>
-    </div>
-
-    @if (account()) {
-      <div class="absolute top-4 left-4 flex justify-center items-center gap-2">
-        <img [src]="account()?.info?.image" class="w-8 h-8">
-        <p class="text-xl">{{ account()?.balance }}</p>
+      <h1 class="text-5xl text-center mb-4">Wallet Operations</h1>
+      <div class="flex justify-center mb-4">
+        <hd-wallet-multi-button></hd-wallet-multi-button>
       </div>
-    }
+
+      @if (account()) {
+        <div class="absolute top-4 left-4 flex justify-center items-center gap-2">
+          <img [src]="account()?.info?.image" class="w-8 h-8">
+          <p class="text-xl">{{ account()?.balance }}</p>
+        </div>
+      }
+
+      <nav>
+        <ul class="flex justify-center items-center gap-4">
+          <li>
+            <a [routerLink]="['']" mat-raised-button>Home</a>
+          </li>
+          <li>
+            <a [routerLink]="['settings']" mat-raised-button>Settings</a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+
+    <main>
+      <router-outlet></router-outlet>
+    </main>
   `,
 })
 export class AppComponent {
---
+++ src/app/home-page.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'nx-heavyduty1-home-page',
+  template: `
+    <h2>Home</h2>
+  `,
+  standalone: true,
+})
+
+export class HomePageComponent {}
