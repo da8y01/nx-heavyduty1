5fec388569db957658b89e541b53d1489aae50c8
Daniel Benitez <da8y_01@yahoo.com>
2024-02-29 10:32:14 -0500

    feat: Adjust styles for Transfer button.

--- src/app/balance-section.component.ts
+++ src/app/balance-section.component.ts
@@ -4,20 +4,24 @@ import { MatDialog } from '@angular/material/dialog';
 import { injectPublicKey } from '@heavy-duty/wallet-adapter';
 import { ShyftApiService } from './shyft-api.service';
 import { TransferModalComponent } from './transfer-modal.component';
+import { MatAnchor } from '@angular/material/button';
 
 @Component({
   selector: 'nx-heavyduty1-balance-section',
   standalone: true,
+  imports: [MatAnchor],
   template: `
     <section>
-      <h2 class="text-lg">Saldo</h2>
+      <h2 class="flex justify-center text-lg">Saldo</h2>
       @if (account()) {
-        <div class="flex gap-2">
+        <div class="flex gap-2 justify-center">
           <img [src]="account()?.info?.image" class="w-8 h-8" />
           <p class="text-xl">{{ account()?.balance }}</p>
         </div>
+        <div class="flex justify-center">
+          <a (click)="openDialog()" mat-raised-button>Transferir</a>
+        </div>
       }
-      <button mat-raised-button (click)="openDialog()">Transferir</button>
     </section>
   `
 })
